aede5c21daff78ffb864444548167501d46345c2
gabriellend <gdvdsn@gmail.com>
2023-06-23 12:58:11 -1000

    Clean up

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
-// play five games of rock, paper, scissors
-// report the outcome at the end (winning, losing, tieing)
-// two players – the computer and a human
-
-// return rock, paper, or scissors randomly
-// I - none
-// O – a string ("Rock", "Paper", or "Scissors")
-// C – skip
-// E – none
 const getComputerChoice = () => {
   const randNum = Math.ceil(Math.random() * 3);
 
   // This does the same thing as the switch statement below
   // just with an if/else statement
   // if (randNum === 1) {
-  //   return "Rock";
+  //   return "rock";
   // } else if (randNum === 2) {
-  //   return "Paper";
+  //   return "paper";
   // } else {
-  //   return "Scissors";
+  //   return "scissors";
   // }
 
   switch (randNum) {
@@ -31,17 +22,15 @@ const getComputerChoice = () => {
   }
 };
 
-// I – none
-// O – a string of the player's choice
-// C – skip
-// E – what if the player hits cancel
 const getPlayerChoice = () => {
   let playerChoice = prompt("Enter 'rock', 'paper', 'scissors' to play.");
+  // If player hits cancel
   if (playerChoice === null) {
     return;
   }
   while (playerChoice === "") {
     playerChoice = prompt("Enter 'rock', 'paper', 'scissors' to play.");
+    // If player hits cancel
     if (playerChoice === null) {
       return;
     }
@@ -50,10 +39,6 @@ const getPlayerChoice = () => {
   return playerChoice;
 };
 
-// I – playerSelection and computerSelection, string
-// O – a string declaring the outcome, i.e. "You win! Rock beats Paper"
-// C – skip
-// E – the case of the player's selection. They could input Rock, rock, RoCk, ROCK, etc.
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
 
@@ -66,8 +51,6 @@ const playRound = (playerSelection, computerSelection) => {
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors");
 
-  // falsy values are values that evaluate to false when coersed into a boolean type
-  // "", 0, undefined, null, false, NaN
   if (playerWins) {
     console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     return "Player wins";
@@ -80,22 +63,15 @@ const playRound = (playerSelection, computerSelection) => {
   }
 };
 
-// game plays 5 single rounds and reports if the player won, lost, or tied overall
-// I – none
-// O – a string reporting the overall outcome
-// C – skip
-// E – what if the player cancels the prompt
 const game = () => {
   let round = 1;
   let playerTotalWins = 0;
   let computerTotalWins = 0;
   let ties = 0;
-  // DRY – Don't Repeat Yourself
 
   while (round <= 5) {
     let computerChoice = getComputerChoice();
     let playerChoice = getPlayerChoice();
-    console.log({ playerChoice });
 
     let outcome = playRound(playerChoice, computerChoice);
     if (outcome === "Player wins") {
