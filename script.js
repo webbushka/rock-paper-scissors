659a1d0c895ecdbff647484b2db50e9723e0a501
AJ Webb <webbushka@gmail.com>
2023-07-06 20:55:00 -1000

    It's so pretty-ish

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const playerScore = document.querySelector('.player-score');
+const robotScore = document.querySelector('.robot-score');
+const btns = document.querySelectorAll('.btn');
+const output = document.querySelector('.output');
+const gameBoard = document.querySelector('.game');
+
+let playerTotalWins = 0;
+let computerTotalWins = 0;
+
 const getComputerChoice = () => {
   const randNum = Math.ceil(Math.random() * 3);
 
@@ -13,12 +22,12 @@ const getComputerChoice = () => {
 
   switch (randNum) {
     case 1:
-      return "rock";
+      return 'rock';
     case 2:
-      return "paper";
+      return 'paper';
     case 3:
     default:
-      return "scissors";
+      return 'scissors';
   }
 };
 
@@ -28,7 +37,7 @@ const getPlayerChoice = () => {
   if (playerChoice === null) {
     return;
   }
-  while (playerChoice === "") {
+  while (playerChoice === '') {
     playerChoice = prompt("Enter 'rock', 'paper', or 'scissors' to play.");
     // If player hits cancel
     if (playerChoice === null) {
@@ -39,74 +48,60 @@ const getPlayerChoice = () => {
   return playerChoice;
 };
 
+const updateOutput = (msg) => {
+  output.innerText = msg;
+};
+
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase();
 
   const playerWins =
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper");
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper');
   const computerWins =
-    (playerSelection === "scissors" && computerSelection === "rock") ||
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors");
+    (playerSelection === 'scissors' && computerSelection === 'rock') ||
+    (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'paper' && computerSelection === 'scissors');
 
   if (playerWins) {
-    console.log(
+    updateOutput(
       `You win this round! ${playerSelection} beats ${computerSelection}.`
     );
-    return "Player wins";
+    return 'Player wins';
   } else if (computerWins) {
-    console.log(
+    updateOutput(
       `You lose this round! ${computerSelection} beats ${playerSelection}.`
     );
-    return "Computer wins";
+    return 'Computer wins';
   } else {
-    console.log(
+    updateOutput(
       `${playerSelection} and ${computerSelection}. It's a tie this round!`
     );
     return "It's a tie";
   }
 };
 
-const game = () => {
-  let playerTotalWins = 0;
-  let computerTotalWins = 0;
-  let ties = 0;
+const game = (evt) => {
+  let computerChoice = getComputerChoice();
+  let playerChoice = evt.target.dataset.type;
 
-  for (let round = 1; round <= 5; round++) {
-    let computerChoice = getComputerChoice();
-    let playerChoice = getPlayerChoice();
-    // If player hits cancel
-    if (!playerChoice) {
-      console.log("Game canceled");
-      return;
-    }
-
-    let outcome = playRound(playerChoice, computerChoice);
-    if (outcome === "Player wins") {
-      playerTotalWins++;
-    } else if (outcome === "Computer wins") {
-      computerTotalWins++;
-    } else {
-      ties++;
-    }
+  let outcome = playRound(playerChoice, computerChoice);
+  if (outcome === 'Player wins') {
+    playerTotalWins++;
+    playerScore.innerText = playerTotalWins;
+  } else if (outcome === 'Computer wins') {
+    computerTotalWins++;
+    robotScore.innerText = computerTotalWins;
   }
 
-  // The above for loop can also be written as a while loop:
-  // let round = 1;
-  // while (round <= 5) {
-  //  ...code to execute
-  //  round ++
-  //}
-
-  if (playerTotalWins > computerTotalWins) {
-    console.log("You win the game! Refresh to play again!");
-  } else if (computerTotalWins > playerTotalWins) {
-    console.log("You lose the game! Refresh to play again!");
-  } else {
-    console.log("The game is a tie! Refresh to play again!");
+  if (playerTotalWins === 5) {
+    gameBoard.innerText = 'You win the game! Refresh to play again!';
+  } else if (computerTotalWins === 5) {
+    gameBoard.innerText = 'You lose the game! Refresh to play again!';
   }
 };
 
-game();
+btns.forEach((btn) => {
+  btn.addEventListener('click', game);
+});
